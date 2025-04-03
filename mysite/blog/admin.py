116ab535f095b38d22619ed0a53119ff3fbe397b
coder6497 <you@example.com>
2025-04-03 18:11:10 +0300

    added modifications to admin panel

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    prepopulated_fields ={'slug': ('title',)}
+    date_hierarchy = 'publish'
+    ordering = ['publish']
