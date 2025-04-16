bf944d4a38e042095838554ff85e9a7f527d67b6
coder6497 <you@example.com>
2025-04-16 23:25:03 +0300

    added email send form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .forms import EmailPostForm
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -26,10 +27,16 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = Post.published.get(id=post_id)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} рекомендует вам прочесть f{post.title}"
+            message = f'Посмотреть {post.title} по адресу {post_url}\n\nСообщение от {cd["name"]}: {cd['comments']}'
+            send_mail(subject, message, 'rty.sem@yandex.ru', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
