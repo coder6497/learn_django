8bc3f158a4502fcb5d7028dfc8befccdc9aba5bc
coder6497 <you@example.com>
2025-04-18 18:48:31 +0300

    added comment support for this post

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,10 +1,18 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
+    name = forms.CharField(max_length=25, label='Имя')
+    email = forms.EmailField(label='От кого')
+    to = forms.EmailField(label='Кому')
     comments = forms.CharField(
         required=False,
-        widget=forms.Textarea
+        widget=forms.Textarea,
+        label='Комментарий'
     )
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.http import Http404
 from django.core.paginator import Paginator
 from django.core.mail import send_mail
-
+from django.views.decorators.http import require_POST
 
 def post_list(request):
     post_list = Post.published.all()
@@ -21,22 +21,41 @@ def post_detail(request, year, month, day, post):
                                     publish__month=month,
                                     publish__day=day
                                 )
+        comments = post.comments.filter(active=True)
+        form = CommentForm()
     except Post.DoesNotExist:
         raise Http404("No Post found")
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, "form": form})
 
 def post_share(request, post_id):
-    post = Post.published.get(id=post_id)
-    sent = False
-    if request.method == 'POST':
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = f"{cd['name']} рекомендует вам прочесть {post.title}"
-            message = f"Посмотреть {post.title} по адресу {post_url}\n\nСообщение от {cd['name']}: {cd['comments']}"
-            send_mail(subject, message, 'rty.sem@yandex.ru', [cd['to']])
-            sent = True
-    else:
-        form = EmailPostForm()
+    try:
+        post = Post.published.get(id=post_id)
+        sent = False
+        if request.method == 'POST':
+            form = EmailPostForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                post_url = request.build_absolute_uri(post.get_absolute_url())
+                subject = f"{cd['name']} рекомендует вам прочесть {post.title}"
+                message = f"Посмотреть {post.title} по адресу {post_url}\n\nСообщение от {cd['name']}: {cd['comments']}"
+                send_mail(subject, message, 'rty.sem@yandex.ru', [cd['to']])
+                sent = True
+        else:
+            form = EmailPostForm()
+    except Post.DoesNotExist:
+        raise Http404("No Post Found")
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    try:
+        post = Post.published.get(id=post_id)
+        comment = None
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+    except Post.DoesNotExist:
+        raise Http404("No Post Found")
+    return render(request, 'blog/post/comments.html', {'post': post, 'form': form, 'comment': comment})
