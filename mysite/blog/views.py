4791fa95a4900db76b9d79580daab71508af42ac
coder6497 <you@example.com>
2025-04-25 21:23:51 +0300

    added tagging for post and custom template tags for display latest posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 from django.http import Http404
 from django.core.paginator import Paginator
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(post_list, 4)
     page_number = request.GET.get('posts', 1)
     posts = paginator.page(page_number)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     try:
@@ -23,9 +29,14 @@ def post_detail(request, year, month, day, post):
                                 )
         comments = post.comments.filter(active=True)
         form = CommentForm()
+
+        post_tags_ids = post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+        similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     except Post.DoesNotExist:
         raise Http404("No Post found")
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, "form": form})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, "form": form, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     try:
